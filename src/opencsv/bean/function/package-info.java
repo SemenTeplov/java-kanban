8e3345b25397362b421d2ac761ad14409a883c26
SemenTeplov <eburg-severca@mail.ru>
2025-06-23 19:05:33 +0500

    Sprint 7 solution решение

--- src/opencsv/bean/function/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package collections function definitions that are used in opencsv.
- * Typically, the JDK-provided functions were insufficient because of
- * exceptions thrown.
- */
-package opencsv.bean.function;
--- src/opencsv/bean/util/package-info.java
+++
@@ -1,2 +0,0 @@
-/** This is a package of utility classes for internal use. */
-package opencsv.bean.util;
