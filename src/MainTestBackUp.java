2f98f756f13d13d0b784c47ef432dc02a023fe1b
SemenTeplov <eburg-severca@mail.ru>
2025-06-23 11:28:30 +0500

    Sprint 7 solution решение

--- src/MainTestBackUp.java
+++ src/MainTestBackUp.java
@@ -5,37 +5,37 @@ import models.AbstractTask;
 public class MainTestBackUp {
     public static void main(String[] args) {
         // Загрузка пустого файла Backup
-        FileBackedTaskManager tManager_origin1 = new FileBackedTaskManager();
-        tManager_origin1.removeAll();
+        FileBackedTaskManager tManagerOrigin1 = new FileBackedTaskManager();
+        tManagerOrigin1.removeAll();
 
         System.out.println("Пустой список");
 
         FileBackedTaskManager.loadFromFile("resourses/backup.csv");
-        FileBackedTaskManager tManager_test1 = new FileBackedTaskManager();
-        printTasks(tManager_test1);
+        FileBackedTaskManager tManagerTest1 = new FileBackedTaskManager();
+        printTasks(tManagerTest1);
 
         System.out.println("-".repeat(20));
 
         // Сохранение нескольких задач и загрузка
-        FileBackedTaskManager tManager_origin2 = new FileBackedTaskManager();
-        tManager_origin2.removeAll();
+        FileBackedTaskManager tManagerOrigin2 = new FileBackedTaskManager();
+        tManagerOrigin2.removeAll();
 
-        tManager_origin2.createEpicTask("Epic 1", "With subtasks");
-        tManager_origin2.createEpicTask("Epic 2", "With subtasks");
-        tManager_origin2.createEpicTask("Epic 3", "With subtasks");
-        tManager_origin2.createEpicTask("Epic 4", "Without subtasks");
+        tManagerOrigin2.createEpicTask("Epic 1", "With subtasks");
+        tManagerOrigin2.createEpicTask("Epic 2", "With subtasks");
+        tManagerOrigin2.createEpicTask("Epic 3", "With subtasks");
+        tManagerOrigin2.createEpicTask("Epic 4", "Without subtasks");
 
         for (int i = 1; i <= 3; i++) {
             for (int l = 1; l <= 3; l++) {
-                tManager_origin2.createSubtask(i, "subtask " + l, "something text");
+                tManagerOrigin2.createSubtask(i, "subtask " + l, "something text");
             }
         }
 
         System.out.println("Полный список");
 
         FileBackedTaskManager.loadFromFile("resourses/backup.csv");
-        FileBackedTaskManager tManager_test2 = new FileBackedTaskManager();
-        printTasks(tManager_test2);
+        FileBackedTaskManager tManagerTest2 = new FileBackedTaskManager();
+        printTasks(tManagerTest2);
 
         System.out.println("-".repeat(20));
 
